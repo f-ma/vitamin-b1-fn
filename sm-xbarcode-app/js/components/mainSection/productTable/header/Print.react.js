e78c470fe177f54cd976723a9dc531e2c1e32f1b
Huy Pham <huypq@smartosc.com>
2015-10-14 13:10:48 +0700

    Print All

--- sm-xbarcode-app/js/components/mainSection/productTable/header/Print.react.js
+++ sm-xbarcode-app/js/components/mainSection/productTable/header/Print.react.js
@@ -2,6 +2,14 @@
 
 var React = require('react');
 var classNames = require('classnames');
+var ProductLockedStore = require('./../../../../stores/ui/ProductLockedAreaStore');
+var ProductStore = require('./../../../../stores/base/ProductStore');
+var PrinterActions = require('./../../../../actions/PrinterActions');
+var BarcodeEncoderConstants = require('./../../../../stores/barcode/constants/EncoderConstant');
+
+function _getLockedProducts() {
+  return ProductLockedStore.getLockedProducts();
+}
 
 var Header = React.createClass({
   propTypes: {
@@ -12,17 +20,34 @@ var Header = React.createClass({
     return null;
   },
 
-
-
   render: function () {
     return (
       <div className={classNames('print')}>
-        <button className={classNames('btn', 'btn-blue')}>
+        <button className={classNames('btn', 'btn-blue')} onClick={this._handleOnClick}>
           <span>PRINT ALL</span>
         </button>
       </div>
     );
+  },
+
+  _handleOnClick: function(event) {
+    event.stopPropagation();
+    var products = _getLockedProducts();
+
+    PrinterActions.show();
+
+    products.forEach(function(product, productIndex) {
+      var barcodeQuantity = parseInt(ProductStore.getLockedQuantity(product));
+      PrinterActions.renderProductBarcode({
+        quantity: barcodeQuantity,
+        product: product,
+        options: {
+          format: BarcodeEncoderConstants.ENCODERS[ProductStore.getLockedSymbology(product)]
+        }
+      });
+    });
   }
+
 });
 
 module.exports = Header;
