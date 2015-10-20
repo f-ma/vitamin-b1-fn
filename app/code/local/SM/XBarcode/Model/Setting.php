d19d707f7bd7cbd7bdc416769af4527ffb38c03f
Huy Quang Pham <huypq@smartosc.com>
2015-10-20 15:34:54 +0700

    Get JSON of all settings

--- app/code/local/SM/XBarcode/Model/Setting.php
+++ app/code/local/SM/XBarcode/Model/Setting.php
@@ -6,4 +6,35 @@ class SM_XBarcode_Model_Setting extends Mage_Core_Model_Abstract
     {
         $this->_init('xBarcode/setting');
     }
+
+    protected function _prepareSettingCollection() {
+      $collection = Mage::getModel('xBarcode/setting')->getCollection();
+      $collection->addFieldToSelect('*');
+
+      return $collection;
+    }
+
+    protected function _prepareSettingData($settingCollection) {
+      $data = array();
+
+      foreach ($settingCollection as $setting) {
+
+        array_push($data, array(
+          "id" => $setting->getId(),
+          "label" => $setting->getLabel(),
+          "code_name" => $setting->getCodeName(),
+          "description" => $setting->getDescription(),
+          "value" => $setting->getValue()
+        ));
+      }
+
+      return $data;
+    }
+
+    public function getJSONAllSettings() {
+        $settingCollection = $this->_prepareSettingCollection()
+        ->setOrder('id', 'DESC');
+
+        return json_encode($this->_prepareSettingData($settingCollection));
+    }
 }
