2c8fa37ddf1a32a7932af815d66e9564b3515bc1
f-ma <fmabroad@gmail.com>
2015-10-22 00:52:06 +0700

    Make sure that initial states will be loaded before rendering application

--- sm-xbarcode-app/js/app.js
+++ sm-xbarcode-app/js/app.js
@@ -8,15 +8,32 @@
  */
 
 global.__ENV = "development";
+global.__URLs = {
+  productIndex: '/index.php/admin/xBarcode_product/index',
+  productFilter: '/index.php/admin/xBarcode_product/filter',
+  orderIndex: '/index.php/admin/xBarcode_order/index',
+  settingIndex: '/index.php/admin/xBarcode_setting/index'
+};
+
+console.log(__URLs);
 
 var React = require('react');
 var XBarcodeApp = require('./components/XBarcodeApp.react');
-var ProductAPI = require('./utils/ProductAPI');
-var ProductStore = require('./stores/base/ProductStore');
+var Data = require('./utils/Data');
 
 var XBarcodeAppComponentsElement = <XBarcodeApp key="app" />;
 var XbarCodeAppContainer = document.getElementById('xbarcodeapp');
 
 //TODO ...
+var XBarcodeAppComponentsInstance = null;
 
-var XBarcodeAppComponentsInstance = React.render(XBarcodeAppComponentsElement, XbarCodeAppContainer);
+(function(window, undefined) {
+  Data.getSettingData(function() {
+    XbarCodeAppContainer.innerHTML = 'Loading initial states...';
+    XbarCodeAppContainer.style.margin = '15px 0 0 15px';
+    setTimeout(function() {
+      XbarCodeAppContainer.style.margin = '0';
+      XBarcodeAppComponentsInstance = React.render(XBarcodeAppComponentsElement, XbarCodeAppContainer);
+    }, 2000);
+  });
+})(window, undefined);
