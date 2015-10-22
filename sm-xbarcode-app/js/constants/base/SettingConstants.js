556ef9bc3bea39a71ba61049358dc305e5e0f8d4
f-ma <fmabroad@gmail.com>
2015-10-22 20:59:45 +0700

    Add Item HtmlIds, CodeName

--- sm-xbarcode-app/js/constants/base/SettingConstants.js
+++ sm-xbarcode-app/js/constants/base/SettingConstants.js
@@ -4,7 +4,8 @@ var keyMirror = require('keymirror');
 var assign = require('object-assign');
 
 module.exports = assign({}, keyMirror({
-  SETTING_GET: null
+  SETTING_GET: null,
+  SETTING_UPDATE: null,
 }), {
   HtmlId: {
     Input: {
@@ -15,7 +16,14 @@ module.exports = assign({}, keyMirror({
     Item: {
       DEFAULT_SYMBOLOGY: 'setting-default-symbology',
       SEARCH_SYMBOLOGY: 'setting-search-symbology',
-      PREVIEW_SYMBOLOGY: 'setting-preview-symbology'
+      PREVIEW_SYMBOLOGY: 'setting-preview-symbology',
+      PRODUCT_BARCODE_GENERATING_ATTRIBUTE: 'setting-product-barcode-generating-attribute'
     }
+  },
+  CodeName: {
+    DEFAULT_GLOBAL_SYMBOLOGY: 'symbology_global_default',
+    DEFAULT_SEARCH_SYMBOLOGY: 'symbology_search_default',
+    DEFAULT_PREVIEW_SYMBOLOGY: 'symbology_preview_default',
+    PRODUCT_BARCODE_GENERATING_ATTRIBUTE: 'product_barcode_generating_attribute'
   }
 });
