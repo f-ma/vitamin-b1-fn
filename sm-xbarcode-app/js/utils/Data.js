8c622750858ddf052afddbbdb91225d4dd5f0282
f-ma <fmabroad@gmail.com>
2015-10-22 21:02:17 +0700

    Changed URLs constant, restructured

--- sm-xbarcode-app/js/utils/Data.js
+++ sm-xbarcode-app/js/utils/Data.js
@@ -11,28 +11,48 @@ var BarcodeConstants = require('./../constants/BarcodeConstants');
 var _url = __URLs;
 
 module.exports = {
-  getSettingData: function(callback) {
-    console.log('Fetching setting data...');
-    var data = [];
+  Setting: {
+    get: function(callback) {
+      console.log('Fetching setting data...');
+      var data = [];
 
-    $.ajax({
-      url: _url.settingIndex,
-      dataType: 'json',
-      type: 'GET',
-      success: function(response) {
-        data = response;
-        DataAction.getSettings(data);
-      },
-      complete: function() {
-        console.log('Fetching setting data completed!');
-        if (typeof callback === 'function') {
-          callback();
+      $.ajax({
+        url: _url.settingIndex,
+        dataType: 'json',
+        type: 'GET',
+        success: function(response) {
+          data = response;
+          DataAction.Setting.get(data);
+        },
+        complete: function() {
+          console.log('Fetching setting data completed!');
+          if (typeof callback === 'function') {
+            callback();
+          }
+        },
+        error: function(xhr, status, err) {
+          console.error(_url.settings, status, err.toString());
         }
-      },
-      error: function(xhr, status, err) {
-        console.error(_url.settings, status, err.toString());
-      }
-    });
+      });
+    },
+
+    update: function(data) {
+      var updatedData = [];
+
+      $.ajax({
+        url: __URLs.Setting.update,
+        dataType: 'json',
+        type: 'GET',
+        data: {id: data.id, val: data.value, fk: window.KEYS.fk},
+        success: function(response) {
+          updatedData = response;
+          DataAction.Setting.update(updatedData);
+        },
+        error: function (xhr, status, err) {
+          console.error(__URLs.Setting.update, status, err.toString());
+        }
+      });
+    }
   },
 
   getProductData: function(filterType, string) {
