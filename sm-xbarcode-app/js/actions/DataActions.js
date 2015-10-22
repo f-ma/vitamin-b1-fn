379ea914b3ee15620e1f4ffebe3ecf23f18c26c0
f-ma <fmabroad@gmail.com>
2015-10-22 20:56:32 +0700

    Added Setting actions, restructured

--- sm-xbarcode-app/js/actions/DataActions.js
+++ sm-xbarcode-app/js/actions/DataActions.js
@@ -6,6 +6,23 @@ var OrderConstants = require('../constants/OrderConstants');
 var SettingConstants = require('../constants/base/SettingConstants');
 
 var DataActions = {
+  Setting: {
+    get: function(data) {
+      AppDispatcher.dispatch({
+        actionType: SettingConstants.SETTING_GET,
+        data: data
+      });
+    },
+    /**
+     * @param: data {id: string, value: string}
+     */
+    update: function(data) {
+      AppDispatcher.dispatch({
+        actionType: SettingConstants.SETTING_UPDATE,
+        data: data
+      });
+    }
+  },
   getProducts: function(data) {
     AppDispatcher.dispatch({
       actionType: ProductConstants.PRODUCT_GET,
@@ -15,12 +32,6 @@ var DataActions = {
   getOrders: function(data) {
     AppDispatcher.dispatch({
       actionType: OrderConstants.ORDER_GET,
-      data: data
-    });
-  },
-  getSettings: function(data) {
-    AppDispatcher.dispatch({
-      actionType: SettingConstants.SETTING_GET,
       data: data
     });
   }
