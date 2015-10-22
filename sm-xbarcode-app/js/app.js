f10b45163b87f22d3f805577f362d6cedfa2faa4
f-ma <fmabroad@gmail.com>
2015-10-22 20:53:21 +0700

    Restructure the global URLs, init setting data before rendering application

--- sm-xbarcode-app/js/app.js
+++ sm-xbarcode-app/js/app.js
@@ -9,31 +9,33 @@
 
 global.__ENV = "development";
 global.__URLs = {
+  Setting: {
+    get: '/index.php/admin/xBarcode_setting/index',
+    update: '/index.php/admin/xBarcode_setting/update'
+  },
   productIndex: '/index.php/admin/xBarcode_product/index',
   productFilter: '/index.php/admin/xBarcode_product/filter',
   orderIndex: '/index.php/admin/xBarcode_order/index',
   settingIndex: '/index.php/admin/xBarcode_setting/index'
 };
 
-console.log(__URLs);
-
-var React = require('react');
-var XBarcodeApp = require('./components/XBarcodeApp.react');
 var Data = require('./utils/Data');
-
-var XBarcodeAppComponentsElement = <XBarcodeApp key="app" />;
 var XbarCodeAppContainer = document.getElementById('xbarcodeapp');
+var SettingStore = require('./stores/base/SettingStore');
 
 //TODO ...
-var XBarcodeAppComponentsInstance = null;
 
 (function(window, undefined) {
-  Data.getSettingData(function() {
+  Data.Setting.get(function() {
     XbarCodeAppContainer.innerHTML = 'Loading initial states...';
     XbarCodeAppContainer.style.margin = '15px 0 0 15px';
     setTimeout(function() {
       XbarCodeAppContainer.style.margin = '0';
-      XBarcodeAppComponentsInstance = React.render(XBarcodeAppComponentsElement, XbarCodeAppContainer);
+
+      var React = require('react');
+      var XBarcodeApp = require('./components/XBarcodeApp.react');
+      var XBarcodeAppComponentsElement = <XBarcodeApp key="app" />;
+      var XBarcodeAppComponentsInstance = React.render(XBarcodeAppComponentsElement, XbarCodeAppContainer);
     }, 2000);
   });
 })(window, undefined);
