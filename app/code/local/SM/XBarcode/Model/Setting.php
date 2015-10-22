7c0163a2989f844d39b34e7e6977e9eaccbe9650
f-ma <fmabroad@gmail.com>
2015-10-21 21:42:50 -0700

    Change JSON response structure

--- app/code/local/SM/XBarcode/Model/Setting.php
+++ app/code/local/SM/XBarcode/Model/Setting.php
@@ -2,29 +2,30 @@
 
 class SM_XBarcode_Model_Setting extends Mage_Core_Model_Abstract
 {
-    protected function _construct()
-    {
-        $this->_init('xBarcode/setting');
-    }
+  protected function _construct()
+  {
+    $this->_init('xBarcode/setting');
+  }
 
-    protected function _prepareSettingCollection() {
-      $collection = Mage::getModel('xBarcode/setting')->getCollection();
-      $collection->addFieldToSelect('*');
+  protected function _prepareSettingCollection() {
+    $collection = Mage::getModel('xBarcode/setting')->getCollection();
+    $collection->addFieldToSelect('*');
 
-      return $collection;
-    }
+    return $collection;
+  }
 
-    protected function _prepareSettingData($settingCollection) {
-      $data = array();
+  protected function _prepareSettingData($settingCollection) {
+    $data = array();
 
-      foreach ($settingCollection as $setting) {
+    foreach ($settingCollection as $setting) {
 
-        array_push($data, array(
-          "id" => $setting->getId(),
+      array_push($data, array(
+        $setting->getCodeName() => array(
+          "id" => $setting->getid(),
           "label" => $setting->getLabel(),
-          "code_name" => $setting->getCodeName(),
           "description" => $setting->getDescription(),
           "value" => $setting->getValue()
+          )
         ));
       }
 
@@ -32,9 +33,9 @@ class SM_XBarcode_Model_Setting extends Mage_Core_Model_Abstract
     }
 
     public function getJSONAllSettings() {
-        $settingCollection = $this->_prepareSettingCollection()
-        ->setOrder('id', 'DESC');
+      $settingCollection = $this->_prepareSettingCollection()
+      ->setOrder('id', 'DESC');
 
-        return json_encode($this->_prepareSettingData($settingCollection));
+      return json_encode($this->_prepareSettingData($settingCollection));
     }
-}
+  }
